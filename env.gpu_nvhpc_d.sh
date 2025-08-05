ad035ce6a5ec56a56baf4cfdd05611122225710e
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-08-05 17:23:26 +0000

    Compile & run using gemv in verints.F90

--- env.gpu_nvhpc_d.sh
+++ env.gpu_nvhpc_d.sh
@@ -1,7 +1,7 @@
 
 module load gcc/nvhpc24.7
 
-export PATH=/home/gmap/mrpm/penigaudn/fxtran-acdc/bin:$PATH
+export PATH=/home/gmap/mrpm/marguina/fxtran-acdc/master+gemm/bin:$PATH
 export PATH=/home/gmap/mrpm/marguina/fxtran/master/bin:$PATH
 
 ulimit -s unlimited
